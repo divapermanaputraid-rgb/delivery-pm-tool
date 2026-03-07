2bf47325e28751b039cab6f14f625dbcd32093b6
divapermanaputraid-rgb <divapermanaputra.id@gmail.com>
2026-03-07 10:24:08 +0700

    feat(api): add global error and not-found middlewares

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import { errorHandler } from "./middlewares/error.middleware.js";
+import { notFoundHandler } from "./middlewares/not-found.middleware.js";
 import router from "./routes/index.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 export default app;
 
